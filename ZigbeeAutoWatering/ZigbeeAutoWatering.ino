12be5ae39329243f022684c0bb5ecfe689f626fd
Juan Pizarro <jpizarrom@gmail.com>
2016-02-11 02:23:12 -0300

    Timer refactor

--- ZigbeeAutoWatering/ZigbeeAutoWatering.ino
+++ ZigbeeAutoWatering/ZigbeeAutoWatering.ino
@@ -1,3 +1,4 @@
+#include <Timer.h>
 #include <XBee.h>
 #include <Printers.h>
 
@@ -7,6 +8,25 @@
 #define MaxTemprature 40  //The Maximum Value of the Temperature
 #define Sensor 1
 #define Carbon 0
+
+boolean isDebugEnabled=true;
+boolean isDebugXbeeEnabled=false;
+
+boolean doUpdate = false;
+boolean doPumpUpdate = false;
+boolean doGetReads = true;
+boolean doCheckEnv = false;
+boolean existReads = false;
+boolean doToggleOn = false;
+
+Timer t;
+int trafficCop =0;  //tracks which station has the right of way (is on)
+
+long stationTime[] = {0,0,0,0,0,0,0,0,0};
+int8_t stationTimer[] = {0,0,0,0,0,0,0,0,0}; 
+int queue[]={0,0,0,0,0,0,0,0,0};  // off: 0, queued: 1, running: 2
+
+boolean isConfigPump = false; // if true, relay8 is used as the pump/master valve switch.  Can be toggled by device tye v2.7 and later
 
 AutoWatering flower;
 
@@ -38,7 +58,7 @@ void zbReceive(ZBRxResponse& rx, uintptr_t) {
   // error occurs, the global onTxStatusResponse handler will print an
   // error message, but no message is printed on succes.
   xbee.send(tx);
-  Serial.println(F("Sending ZBTxRequest"));
+  if (isDebugXbeeEnabled) Serial.println(F("Sending ZBTxRequest"));
 }
 
 void receive16(Rx16Response& rx, uintptr_t) {
@@ -53,7 +73,7 @@ void receive16(Rx16Response& rx, uintptr_t) {
   // error occurs, the global onTxStatusResponse handler will print an
   // error message, but no message is printed on succes.
   xbee.send(tx);
-  Serial.println(F("Sending Tx16Request"));
+  if (isDebugXbeeEnabled) Serial.println(F("Sending Tx16Request"));
 }
 
 void receive64(Rx64Response& rx, uintptr_t) {
@@ -68,17 +88,17 @@ void receive64(Rx64Response& rx, uintptr_t) {
   // error occurs, the global onTxStatusResponse handler will print an
   // error message, but no message is printed on succes.
   xbee.send(tx);
-  Serial.println(F("Sending Tx64Request"));
+  if (isDebugXbeeEnabled)  Serial.println(F("Sending Tx64Request"));
 }
 void receiveZBExplicitRx(ZBExplicitRxResponse& rx, uintptr_t) {
-  printResponseCb(rx, (uintptr_t)(Print*)&Serial);
+  if (isDebugXbeeEnabled) printResponseCb(rx, (uintptr_t)(Print*)&Serial);
   
 //  uint8_t *payload_rx = rx.getFrameData() + rx.getDataOffset();
 
-  Serial.println(F("Home Automation Profile"));
+  if (isDebugXbeeEnabled) Serial.println(F("Home Automation Profile"));
 
   if (rx.getClusterId() == 0x0004){
-    Serial.println(F("Simple Descriptor Request"));
+    if (isDebugXbeeEnabled) Serial.println(F("Simple Descriptor Request"));
       XBeeAddress64 &addr64 = rx.getRemoteAddress64();
       uint16_t addr16 = rx.getRemoteAddress16();
       uint8_t broadcastRadius = 0;
@@ -126,7 +146,7 @@ void receiveZBExplicitRx(ZBExplicitRxResponse& rx, uintptr_t) {
       xbee.send(tx);
   }
   if (rx.getClusterId() == 0x0005){
-    Serial.println(F("Active Endpoints Request"));
+    if (isDebugXbeeEnabled) Serial.println(F("Active Endpoints Request"));
       XBeeAddress64 &addr64 = rx.getRemoteAddress64();
       uint16_t addr16 = rx.getRemoteAddress16();
       uint8_t broadcastRadius = 0;
@@ -166,35 +186,35 @@ void receiveZBExplicitRx(ZBExplicitRxResponse& rx, uintptr_t) {
   
   // BasicCluster
   if (rx.getClusterId() == 0x0000){
-    Serial.println(F("BasicCluster"));
+    if (isDebugXbeeEnabled) Serial.println(F("BasicCluster"));
     cluster_cmd_req_header_t *rsp = (cluster_cmd_req_header_t*)(rx.getFrameData() + rx.getDataOffset());
-    printHex(Serial, rsp->frame_control);
-    Serial.println();
-    printHex(Serial, rsp->sequence_number);
-    Serial.println();
-    printHex(Serial, rsp->command);
-    Serial.println();
+    if (isDebugXbeeEnabled) printHex(Serial, rsp->frame_control);
+    if (isDebugXbeeEnabled) Serial.println();
+    if (isDebugXbeeEnabled) printHex(Serial, rsp->sequence_number);
+    if (isDebugXbeeEnabled) Serial.println();
+    if (isDebugXbeeEnabled) printHex(Serial, rsp->command);
+    if (isDebugXbeeEnabled) Serial.println();
     
     if(rsp->command == 0x00){
-      Serial.println(F("Read attributes"));
+      if (isDebugXbeeEnabled) Serial.println(F("Read attributes"));
       uint8_t *attributes = ((uint8_t*)rsp + sizeof(cluster_cmd_req_header_t));
       uint16_t attribute = (uint16_t)attributes[1] << 8 | attributes[0];
       
-      Serial.print(F("Reading 0x"));
-      printHex(Serial, attributes[0]);
-      Serial.println();
-      printHex(Serial, attributes[1]);
-      Serial.println();
+      if (isDebugXbeeEnabled) Serial.print(F("Reading 0x"));
+      if (isDebugXbeeEnabled) printHex(Serial, attributes[0]);
+      if (isDebugXbeeEnabled) Serial.println();
+      if (isDebugXbeeEnabled) printHex(Serial, attributes[1]);
+      if (isDebugXbeeEnabled) Serial.println();
       
-      Serial.print(F("Reading 0x"));
-      printHex(Serial, attribute);
-      Serial.println();
+      if (isDebugXbeeEnabled) Serial.print(F("Reading 0x"));
+      if (isDebugXbeeEnabled) printHex(Serial, attribute);
+      if (isDebugXbeeEnabled) Serial.println();
 
       if(attribute == 0x0000){
-        Serial.println(F("Reading ZCL_VERSION"));
+        if (isDebugXbeeEnabled) Serial.println(F("Reading ZCL_VERSION"));
       }
       if(attribute == 0x0001){
-        Serial.println(F("Reading APPLICATION_VERSION"));
+        if (isDebugXbeeEnabled) Serial.println(F("Reading APPLICATION_VERSION"));
       }
       uint8_t payload[] = {0x00, rsp->sequence_number, 0x01, attributes[0], attributes[1], 0x00, 0x20, 0x03};
       
@@ -213,7 +233,7 @@ void receiveZBExplicitRx(ZBExplicitRxResponse& rx, uintptr_t) {
     }
   }
   if (rx.getClusterId() == 0x0402){
-    Serial.println(F("TemperatureMeasurementCluster"));
+    if (isDebugXbeeEnabled) Serial.println(F("TemperatureMeasurementCluster"));
     cluster_cmd_req_header_t *rsp = (cluster_cmd_req_header_t*)(rx.getFrameData() + rx.getDataOffset());
     if(rsp->command == 0x00){
       XBeeAddress64 &addr64 = rx.getRemoteAddress64();
@@ -230,16 +250,16 @@ void receiveZBExplicitRx(ZBExplicitRxResponse& rx, uintptr_t) {
       uint16_t attribute = (uint16_t)attributes[1] << 8 | attributes[0];
       
       if(attribute == 0x0000){
-        Serial.println(F("Reading MeasuredValue"));
+        if (isDebugXbeeEnabled) Serial.println(F("Reading MeasuredValue"));
       }
       if(attribute == 0x0001){
-        Serial.println(F("Reading MinMeasuredValue"));
+        if (isDebugXbeeEnabled) Serial.println(F("Reading MinMeasuredValue"));
       }
       if(attribute == 0x0002){
-        Serial.println(F("Reading MaxMeasuredValue"));
+        if (isDebugXbeeEnabled) Serial.println(F("Reading MaxMeasuredValue"));
       }
       if(attribute == 0x0003){
-        Serial.println(F("Reading Tolerance"));
+        if (isDebugXbeeEnabled) Serial.println(F("Reading Tolerance"));
       }
       uint8_t payload[] = {0x00, rsp->sequence_number, 0x01, attributes[0], attributes[1], 0x00, 0x20, (uint8_t)temperature};
       ZBExplicitTxRequest tx(addr64,addr16, broadcastRadius, option, payload, sizeof(payload), frameId, srcEndpoint, dstEndpoint, clusterId, profileId);
@@ -247,7 +267,7 @@ void receiveZBExplicitRx(ZBExplicitRxResponse& rx, uintptr_t) {
     }
   }
   if (rx.getClusterId() == 0x0405){
-    Serial.println(F("RelativeHumidityMeasurementCluster"));
+    if (isDebugXbeeEnabled) Serial.println(F("RelativeHumidityMeasurementCluster"));
     cluster_cmd_req_header_t *rsp = (cluster_cmd_req_header_t*)(rx.getFrameData() + rx.getDataOffset());
     if(rsp->command == 0x00){
       XBeeAddress64 &addr64 = rx.getRemoteAddress64();
@@ -264,16 +284,16 @@ void receiveZBExplicitRx(ZBExplicitRxResponse& rx, uintptr_t) {
       uint16_t attribute = (uint16_t)attributes[1] << 8 | attributes[0];
       
       if(attribute == 0x0000){
-        Serial.println(F("Reading MeasuredValue"));
+        if (isDebugXbeeEnabled) Serial.println(F("Reading MeasuredValue"));
       }
       if(attribute == 0x0001){
-        Serial.println(F("Reading MinMeasuredValue"));
+        if (isDebugXbeeEnabled) Serial.println(F("Reading MinMeasuredValue"));
       }
       if(attribute == 0x0002){
-        Serial.println(F("Reading MaxMeasuredValue"));
+        if (isDebugXbeeEnabled) Serial.println(F("Reading MaxMeasuredValue"));
       }
       if(attribute == 0x0003){
-        Serial.println(F("Reading Tolerance"));
+        if (isDebugXbeeEnabled) Serial.println(F("Reading Tolerance"));
       }
       uint8_t payload[] = {0x00, rsp->sequence_number, 0x01, attributes[0], attributes[1], 0x00, 0x20, (uint8_t)humidity};
       ZBExplicitTxRequest tx(addr64,addr16, broadcastRadius, option, payload, sizeof(payload), frameId, srcEndpoint, dstEndpoint, clusterId, profileId);
@@ -281,7 +301,7 @@ void receiveZBExplicitRx(ZBExplicitRxResponse& rx, uintptr_t) {
     }
   }
   if (rx.getClusterId() == 0x0B04){
-    Serial.println(F("ElectricalMeasurementCluster"));
+    if (isDebugXbeeEnabled) Serial.println(F("ElectricalMeasurementCluster"));
     cluster_cmd_req_header_t *rsp = (cluster_cmd_req_header_t*)(rx.getFrameData() + rx.getDataOffset());
     if(rsp->command == 0x00){
       XBeeAddress64 &addr64 = rx.getRemoteAddress64();
@@ -298,7 +318,7 @@ void receiveZBExplicitRx(ZBExplicitRxResponse& rx, uintptr_t) {
       uint16_t attribute = (uint16_t)attributes[1] << 8 | attributes[0];
       
       if(attribute == 0x0000){
-        Serial.println(F("Reading MeasurementType"));
+        if (isDebugXbeeEnabled) Serial.println(F("Reading MeasurementType"));
       }
       uint8_t payload[] = {0x00, rsp->sequence_number, 0x01, attributes[0], attributes[1], 0x00, 0x20, (uint8_t)moisture_dat};
       ZBExplicitTxRequest tx(addr64,addr16, broadcastRadius, option, payload, sizeof(payload), frameId, srcEndpoint, dstEndpoint, clusterId, profileId);
@@ -306,14 +326,15 @@ void receiveZBExplicitRx(ZBExplicitRxResponse& rx, uintptr_t) {
     }
   }
 }
+
 void setup() {
   flower.Initialization();//Initialization for the watering kit
   
-  Serial.begin(9600);
+  if (isDebugEnabled || isDebugXbeeEnabled) Serial.begin(9600);
 //  while (!Serial) {
 //    ; // wait for serial port to connect. Needed for native USB port only
 //  }
-//  Serial.println("Goodnight moon!");
+  if (isDebugEnabled || isDebugXbeeEnabled) Serial.println("Goodnight moon!");
 
   Serial1.begin(9600);
   xbee.setSerial(Serial1);
@@ -321,9 +342,9 @@ void setup() {
   // Make sure that any errors are logged to Serial. The address of
   // Serial is first cast to Print*, since that's what the callback
   // expects, and then to uintptr_t to fit it inside the data parameter.
-  xbee.onPacketError(printErrorCb, (uintptr_t)(Print*)&Serial);
-  xbee.onTxStatusResponse(printErrorCb, (uintptr_t)(Print*)&Serial);
-  xbee.onZBTxStatusResponse(printErrorCb, (uintptr_t)(Print*)&Serial);
+  if (isDebugXbeeEnabled) xbee.onPacketError(printErrorCb, (uintptr_t)(Print*)&Serial);
+  if (isDebugXbeeEnabled) xbee.onTxStatusResponse(printErrorCb, (uintptr_t)(Print*)&Serial);
+  if (isDebugXbeeEnabled) xbee.onZBTxStatusResponse(printErrorCb, (uintptr_t)(Print*)&Serial);
 
   // These are called when an actual packet received
   xbee.onZBRxResponse(zbReceive);
@@ -332,22 +353,164 @@ void setup() {
   xbee.onZBExplicitRxResponse(receiveZBExplicitRx);
 
   // Print any unhandled response with proper formatting
-  xbee.onOtherResponse(printResponseCb, (uintptr_t)(Print*)&Serial);
+  if (isDebugXbeeEnabled) xbee.onOtherResponse(printResponseCb, (uintptr_t)(Print*)&Serial);
 
   // Enable this to print the raw bytes for _all_ responses before they
   // are handled
-  xbee.onResponse(printRawResponseCb, (uintptr_t)(Print*)&Serial);
+  if (isDebugXbeeEnabled) xbee.onResponse(printRawResponseCb, (uintptr_t)(Print*)&Serial);
+
+  // setup timed actions 
+  t.every(1L*60L*1000L, queueManager);// double check queue to see if there is work to do
+//  t.every(10L*60L*1000L, timeToUpdate); //send update to smartThings hub every 10 min
+  t.every(1L*60L*1000L, timeToGetReads); //
+  t.every(1L*60L*1000L, timeToCheckEnv); //
 
 }
 
+void timeToCheckEnv() {
+  if (isDebugEnabled) Serial.println(F("timeToCheckEnv"));
+  scheduleCheckEnv();
+}
+void scheduleCheckEnv() {
+  if (isDebugEnabled) Serial.println(F("scheduleCheckEnv"));
+  doCheckEnv = true; 
+}
+void CheckEnv() {
+  if (isDebugEnabled) Serial.println(F("CheckEnv"));
+  int humidity_max;
+  humidity_max = flower.ADJ_humMax(); 
+  if(existReads && trafficCop==0 && moisture_dat<=humidity_max && temperature<=MaxTemprature){
+    if (isDebugEnabled) Serial.println(F("enqueue"));
+    int addStation=1;
+    queue[addStation]=1;
+    stationTime[addStation]=4L*1000L;
+  }
+}
+
+void timeToUpdate() {
+  if (isDebugEnabled) Serial.println(F("timeToUpdate"));
+//  scheduleUpdate();
+//  schedulePumpUpdate();
+}
+void timeToGetReads() {
+  if (isDebugEnabled) Serial.println(F("timeToGetReads"));
+//  scheduleUpdate();
+  scheduleGetReads();
+}
+void scheduleUpdate() {
+  if (isDebugEnabled) Serial.println(F("scheduleUpdate"));
+  doUpdate = true; 
+}
+void scheduleGetReads() {
+  if (isDebugEnabled) Serial.println(F("scheduleGetReads"));
+  doGetReads = true;
+}
+
+void GetReads() {
+  if (isDebugEnabled) Serial.println(F("GetReads"));
+  temperature = flower.getTemperature();
+  moisture_dat = flower.MoistureSensor();
+  humidity = flower.getHumidity();
+  Serial.print("Temperature is :");
+  Serial.println(temperature);
+  Serial.print("Soil Moisture is :");
+  Serial.println(moisture_dat);
+  Serial.print("Humidity is :");
+  Serial.println(humidity);
+  existReads = true;
+}
+
+void schedulePumpUpdate() {
+  doPumpUpdate = true;
+}
+void sendPumpUpdate() {
+  if (isDebugEnabled) Serial.println(F("sendPumpUpdate"));
+}
+
+void queueManager() 
+{
+  Serial.print("trafficCop is :");
+  Serial.println(trafficCop);
+  Serial.print("queue[1] is :");
+  Serial.println(queue[1]);
+  Serial.print("stationTimer[1] is :");
+  Serial.println(stationTimer[1]);
+  Serial.print("stationTime[1] is :");
+  Serial.println(stationTime[1]);
+  
+  if (isDebugEnabled) Serial.println(F("queueManager"));
+  int i=1;
+  while (i<2) {
+    if (trafficCop==0 && queue[i]==1) {
+      //ready for next in line 
+      trafficCop=i;
+      scheduleToggleOn();
+    }
+    i++;
+  }
+}
+void scheduleToggleOn() {
+  if (isDebugEnabled) Serial.println(F("scheduleToggleOn"));
+  doToggleOn = true;
+}
+void toggleOn() {
+  if (isDebugEnabled) Serial.println(F("toggleOn"));
+  queue[trafficCop]=2;
+
+  digitalWrite(6,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(6,HIGH);
+  digitalWrite(5,HIGH);
+
+  Serial.print("stationTimer is :");
+  Serial.println(stationTimer[trafficCop]);
+  Serial.print("stationTime is :");
+  Serial.println(stationTime[trafficCop]);
+  if (stationTimer[trafficCop]>0)
+    t.stop(stationTimer[trafficCop]); // Kill any previously started timers.
+  Serial.print("stationTimer is :");
+  Serial.println(stationTimer[trafficCop]);
+  Serial.print("stationTime is :");
+  Serial.println(stationTime[trafficCop]);
+  stationTimer[trafficCop] = t.after (stationTime[trafficCop], toggleOff);
+  Serial.print("stationTimer is :");
+  Serial.println(stationTimer[trafficCop]);
+  Serial.print("stationTime is :");
+  Serial.println(stationTime[trafficCop]);
+}
+void toggleOff() {
+  if (isDebugEnabled) Serial.println(F("toggleOff"));
+  flower.pumpOff();
+  queue[trafficCop]=0; //remove relay from from queue 
+  trafficCop=0; //ready to check queue or watch for new commmands
+}
+
 void loop() {
+  //run timer 
+  t.update();
+
+  if (doToggleOn){
+    toggleOn();
+    doToggleOn=false;
+  }
+
+  if (doCheckEnv) {
+    CheckEnv();
+    doCheckEnv = false;
+  }
+
+  if (doPumpUpdate) {
+    sendPumpUpdate();
+    doPumpUpdate = false;
+  }
+  if (doGetReads) {
+    GetReads();
+    doGetReads = false;
+  }
+  
   // Continuously let xbee read packets and call callbacks.
   xbee.loop();
-  temperature = flower.getTemperature();
-  xbee.loop();
-  moisture_dat = flower.MoistureSensor();
-  xbee.loop();
-  humidity = flower.getHumidity();
-  
 }
 
